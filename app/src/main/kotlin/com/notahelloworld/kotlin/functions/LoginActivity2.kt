148cfbba867f84b017533294f804b89bc5c5fedd
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2017-06-10 08:56:04 -0300

    Add inline example

--- app/src/main/kotlin/com/notahelloworld/kotlin/functions/LoginActivity2.kt
+++ app/src/main/kotlin/com/notahelloworld/kotlin/functions/LoginActivity2.kt
@@ -1,9 +1,11 @@
 package com.notahelloworld.kotlin.functions
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import com.notahelloworld.kotlin.R
+import com.notahelloworld.kotlin.nullable.UserActivity1
 
 class LoginActivity2 : Activity() {
 
@@ -13,7 +15,17 @@ class LoginActivity2 : Activity() {
     super.onCreate(savedInstanceState)
 
     val height = getHeight()
+
+    button.height = height
+
+    navigate<UserActivity1>("123")
   }
 
   fun getHeight() = button.height
+
+  inline fun <reified T : Activity> Activity.navigate(id: String) {
+    val intent = Intent(this, T::class.java)
+    intent.putExtra("id", id)
+    startActivity(intent)
+  }
 }
